988e5afc21f8ad4c8e7cbfd400a1b718efe32c5c
roccomuso <rocco@hackerstribe.com>
2017-02-26 19:34:50 +0100

    path bug fix

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 // Routes autoloader
 
 var fs = require('fs')
+var path = require('path')
 var debug = require('debug')('api-gateway:routesLoader')
 
 module.exports = function(app){
@@ -9,7 +10,7 @@ module.exports = function(app){
       return (file.indexOf('.') !== 0) && (file !== 'index.js')
     })
     .forEach(function(file){
-      require(file)(app)
+      require(path.join(__dirname, file))(app)
       debug('Route', file, 'loaded')
     })
 }
