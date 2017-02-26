7165fbbb07c9b81aeb7dd856be04a791e32d2947
roccomuso <rocco@hackerstribe.com>
2017-02-26 22:36:55 +0100

    code standardized

--- routes/example.js
+++ routes/example.js
@@ -1,41 +1,41 @@
-var proxy = require("http-proxy-middleware")
+var proxy = require('http-proxy-middleware')
 
 /**
  * Proxy options
  */
 var options = {
     // hostname to the target server
-    target: 'http://localhost:3000',
+  target: 'http://localhost:3000',
 
     // set correct host headers for name-based virtual hosted sites
-    changeOrigin: true,
+  changeOrigin: true,
 
     // enable websocket proxying
-    ws: true,
+  ws: true,
 
     // additional request headers
-    headers: {
-        'x-powered-by': 'api-gateway'
-    },
+  headers: {
+    'x-powered-by': 'api-gateway'
+  },
 
     // rewrite paths
-    pathRewrite: {
-        '^/api/old-path' : '/api/new-path',     // rewrite path
-        '^/api/remove/path' : '/path'           // remove base path
-    },
+  pathRewrite: {
+    '^/api/old-path': '/api/new-path',     // rewrite path
+    '^/api/remove/path': '/path'           // remove base path
+  },
 
     // re-target based on the request's host header and/or path
-    router: {
+  router: {
       // host[/path]                 :  <new target>
       // /path                       :  <new target>
-        'integration.localhost:8000' : 'http://localhost:8001',  // host only
-        'staging.localhost:8000'     : 'http://localhost:8002',  // host only
-        'localhost:8000/api'         : 'http://localhost:8003',  // host + path
-        '/rest'                      : 'http://localhost:8004'   // path only
-    },
+    'integration.localhost:8000': 'http://localhost:8001',  // host only
+    'staging.localhost:8000': 'http://localhost:8002',  // host only
+    'localhost:8000/api': 'http://localhost:8003',  // host + path
+    '/rest': 'http://localhost:8004'   // path only
+  },
 
     // control logging
-    logLevel: 'silent',
+  logLevel: 'silent',
 
     // use a different lib for logging;
     // i.e., write logs to file or server
@@ -46,22 +46,22 @@ var options = {
     */
 
     // subscribe to http-proxy's error event
-    onError: function onError(err, req, res) {
-        res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.end('Something went wrong.');
-    },
+  onError: function onError (err, req, res) {
+    res.writeHead(500, {'Content-Type': 'text/plain'})
+    res.end('Something went wrong. ' + err.toString())
+  },
 
     // subscribe to http-proxy's proxyRes event
-    onProxyRes: function (proxyRes, req, res) {
-        //proxyRes.headers['x-added'] = 'foobar'; // add header to the response
-        //delete proxyRes.headers['x-removed']; // delete response header
-    },
+  onProxyRes: function (proxyRes, req, res) {
+        // proxyRes.headers['x-added'] = 'foobar'; // add header to the response
+        // delete proxyRes.headers['x-removed']; // delete response header
+  },
 
     // subscribe to http-proxy's proxyReq event
-    onProxyReq: function (proxyReq, req, res) {
+  onProxyReq: function (proxyReq, req, res) {
         // add custom header to request
-        proxyReq.setHeader('x-forwarded-for', req.connection.remoteAddress);
-    }
+    proxyReq.setHeader('x-forwarded-for', req.connection.remoteAddress)
+  }
 
     /**
      * The following options are provided by Nodejitsu's http-proxy
@@ -91,6 +91,6 @@ var options = {
  * Create the proxy middleware, so it can be used in a server.
  */
 
-module.exports = function(app){
+module.exports = function (app) {
   app.use('/example', proxy(options))
 }
--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ var fs = require('fs')
 var path = require('path')
 var debug = require('debug')('api-gateway:routesLoader')
 
-module.exports = function(app){
+module.exports = function (app) {
   fs.readdirSync(__dirname)
-    .filter(function(file) {
+    .filter(function (file) {
       return (file.indexOf('.') !== 0) && (file !== 'index.js')
     })
-    .forEach(function(file){
+    .forEach(function (file) {
       require(path.join(__dirname, file))(app)
       debug('Route', file, 'loaded')
     })
