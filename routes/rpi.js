002de2ff5ae2e856c72503e90697fd5b751fd2c4
roccomuso <rocco@hackerstribe.com>
2017-02-27 00:13:51 +0100

    testing entry-points

--- routes/rpi.js
+++ routes/rpi.js
@@ -1,5 +1,10 @@
 var proxy = require('http-proxy-middleware')
 
-module.exports = function(app){
-  app.use('/api', proxy({target: 'http://192.168.88.111:8080', changeOrigin: true, ws: true}))
+module.exports = function (app) {
+  // app.use('/iot', proxy({target: 'http://192.168.88.111:8080', changeOrigin: true, ws: true}))
+  app.use('/rpi', proxy({target: 'http://192.168.88.111', onError: function onError (err, req, res) {
+    res.writeHead(500, {'Content-Type': 'text/plain'})
+    res.end(err.toString())
+  }}))
+  // app.use('/rpi', function(req, res, next){ res.end('ciaooo');})
 }
