c7709bad629883e0d2a3c4cb1fac1dda1eb8b275
roccomuso <rocco@hackerstribe.com>
2017-03-05 17:58:41 +0100

    pathRewrite for a correct map

--- routes/rpi.js
+++ routes/rpi.js
@@ -1,10 +1,20 @@
 var proxy = require('http-proxy-middleware')
+var transformerProxy = require('transformer-proxy')
+
+function isPathAbsolute(path) {
+  return /^(?:\/|[a-z]+:\/\/)/.test(path);
+}
+
+function transformerFunction(data, req, res){
+  console.log(data.toString()); return data
+  var page = data.toString().replace(/href="\//g, 'href="/iot/')
+  page = page.toString().replace(/src="\//g, 'src="/iot/')
+  return new Buffer(page)
+}
 
 module.exports = function (app) {
-  // app.use('/iot', proxy({target: 'http://192.168.88.111:8080', changeOrigin: true, ws: true}))
-  app.use('/rpi', proxy({target: 'http://192.168.88.111', onError: function onError (err, req, res) {
-    res.writeHead(500, {'Content-Type': 'text/plain'})
-    res.end(err.toString())
-  }}))
+  app.use(transformerProxy(transformerFunction))
+  app.use('/iot', proxy({target: 'http://192.168.88.111:8080', pathRewrite: {'^/iot' : '/'}, changeOrigin: true, ws: true, logProvider: function(){ return require('debug')('api-gateway:proxyLog')}}))
+  app.use('/rpi', proxy({target: 'http://192.168.88.111', pathRewrite: {'^/rpi' : '/'}, changeOrigin: true }))
   // app.use('/rpi', function(req, res, next){ res.end('ciaooo');})
 }
